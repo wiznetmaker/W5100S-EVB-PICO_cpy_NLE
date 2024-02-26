fb2109e78038c1bbf4c8684bc482ea7aac2d0ce7
WIZnet HK Ron <ronpang@wiznet.hk>
2024-02-26 11:12:30 +0800

    Add files via upload

---
+++ github_secrets.py
@@ -0,0 +1,13 @@
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
+
+# This file is where you keep secret settings, passwords, and tokens!
+# If you put them in the code you risk committing that info or sharing it
+
+secrets = {
+    "NLE_user": "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
+    "NLE_pass": "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
+    "NLE_id": "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
+    "NLE_url": "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
+    "NLE_pub": "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
+}
